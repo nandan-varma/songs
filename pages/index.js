f15cbf035eb2f01a3fa6f70488e49fe4883c819c
Nandan Varma <nandanvarma.me@gmail.com>
2023-05-25 18:56:08 +0530

    added icons and improved links

--- pages/index.js
+++ pages/index.js
@@ -2,62 +2,104 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faPlus, faMagnifyingGlass, faDownload } from '@fortawesome/free-solid-svg-icons'
-const AlbumPage = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPlus, faMagnifyingGlass, faDownload } from '@fortawesome/free-solid-svg-icons';
+
+const Search = () => {
     const router = useRouter();
 
     const [playlist, setPlaylist] = useState([]);
     const [currentSongIndex, setCurrentSongIndex] = useState(0);
     const [searchQuery, setSearchQuery] = useState('');
     const [searchResults, setSearchResults] = useState([]);
+    const [searchType, setSearchType] = useState('song'); // 'song' or 'album'
 
     useEffect(() => {
         if (searchQuery) {
-            fetch(`https://jiosaavn-api-ebon-one.vercel.app/search/songs?query=${searchQuery}`)
+            let searchUrl = '';
+            if (searchType === 'song') {
+                searchUrl = `https://saavn.me/search/songs?query=${searchQuery}`;
+            } else if (searchType === 'album') {
+                searchUrl = `https://saavn.me/search/albums?query=${searchQuery}`;
+            }
+
+            fetch(searchUrl)
                 .then(response => response.json())
                 .then(data => {
-                    if (data.data) {
-                        setSearchResults(data.data.results)
+                    if (data.data && data.data.results) {
+                        setSearchResults(data.data.results);
+                    } else {
+                        setSearchResults([]);
                     }
-                    else {
-                        setSearchResults([])
-                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching search results:', error);
+                    setSearchResults([]);
                 });
         }
-    }, [searchQuery]);
+    }, [searchQuery, searchType]);
 
-    if (!searchResults) return <div>Nothing to show</div>;
+    const handlePlay = item => {
+        let song;
+        if (searchType === 'song') {
+            song = item;
+            setPlaylist([song]);
+            setCurrentSongIndex(0);
+        } else if (searchType === 'album') {
+            console.log(item)
+            router.push(`album/${item.id}`)
+            // For albums, select the first song to play
+            // song = item.songs[0];
+        }
 
-    const handlePlay = song => {
-        console.log(song)
-        setPlaylist([song]);
-        setCurrentSongIndex(0);
     };
 
-    const handleAddToPlaylist = song => {
-        setPlaylist(prevPlaylist => [...prevPlaylist, song]);
+    const handleAddToPlaylist = item => {
+        let songs;
+        if (searchType === 'song') {
+            songs = [item];
+        } else if (searchType === 'album') {
+            songs = item.songs;
+        }
+
+        setPlaylist(prevPlaylist => [...prevPlaylist, ...songs]);
     };
 
     const handleNextSong = () => {
         setCurrentSongIndex(prevIndex => (prevIndex + 1) % playlist.length);
     };
 
+    const handlePrevSong = () => {
+        if(prevIndex > 0){
+        setCurrentSongIndex(prevIndex => (prevIndex - 1) % playlist.length);
+        }
+    };
+
+    const handleSearchTypeChange = e => {
+        setSearchType(e.target.value);
+        setSearchQuery('');
+    };
+
     return (
         <>
             <div className='main-search'>
-                <input className='bordered' type="text" onChange={e => setSearchQuery(e.target.value)} />
+                <input className='bordered' type="text" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
+                <select name="type" id="type" value={searchType} onChange={handleSearchTypeChange}>
+                    <option value="song">Song</option>
+                    <option value="album">Album</option>
+                </select>
                 <FontAwesomeIcon icon={faMagnifyingGlass} />
                 <ul className='songs'>
-                    {searchResults.map(song => (
-                        <li className='song bordered' key={song.id}>
-                            <img className='song-art bordered' src={song.image.find(img => img.quality === '500x500').link} alt={song.name} />
-                            <h3>{song.name}</h3>
-                            <h4>{song.primaryArtists}</h4>
+                    {searchResults.map(result => (
+                        <li className='song bordered' key={result.id}>
+                            <img className='song-art bordered' src={result.image.find(img => img.quality === '500x500').link} alt={result.name} />
+                            <h3>{result.name}</h3>
+                            {/* {searchType === 'song' && <h4>{result.primaryArtists}</h4>}
+                            {searchType === 'album' && <h4>{result.primaryArtists[0]?.name}</h4>} */}
                             <div className='song-controls'>
-                                <FontAwesomeIcon onClick={() => handlePlay(song)} icon={faPlay} />
-                                <FontAwesomeIcon onClick={() => handleAddToPlaylist(song)} icon={faPlus} />
-                                <FontAwesomeIcon onClick={() => handleAddToPlaylist(song)} icon={faDownload} />
+                                <FontAwesomeIcon onClick={() => handlePlay(result)} icon={faPlay} />
+                                <FontAwesomeIcon onClick={() => handleAddToPlaylist(result)} icon={faPlus} />
+                                <FontAwesomeIcon onClick={() => handleAddToPlaylist(result)} icon={faDownload} />
                             </div>
                         </li>
                     ))}
@@ -65,17 +107,20 @@ const AlbumPage = () => {
             </div>
             {playlist.length > 0 && (
                 <div style={{ position: 'fixed', bottom: 0, width: '100%' }}>
-                    <AudioPlayer
-                        src={playlist[currentSongIndex].downloadUrl.find(url => url.quality === '160kbps').link}
+                <AudioPlayer
+                        autoPlay
+                        src={playlist[currentSongIndex].downloadUrl[4].link}
                         onEnded={handleNextSong}
                         showSkipControls
                         onClickNext={handleNextSong}
+                        onClickPrevious={handlePrevSong}
+                        showJumpControls={false}
+                        layout='horizontal-reverse'
                     />
                 </div>
             )}
-
         </>
     );
 };
 
-export default AlbumPage;
+export default Search;

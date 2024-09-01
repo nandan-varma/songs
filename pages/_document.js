1481f251680d7197deca6b8f2666f5966de1b842
Nandan Varma <74198723+nandan-varma@users.noreply.github.com>
2024-09-01 18:28:14 +0000

    added analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script defer data-domain="songs.nandanvarma.com" src="https://plausible.nandanvarma.com/js/script.js"></script>
       </Head>
       <body>
         <Main />

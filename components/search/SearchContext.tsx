1ae3ee144f345b53a3977630e650e4361a189758
Nandan Varma <74198723+nandan-varma@users.noreply.github.com>
2025-02-03 23:40:55 +0000

    fixed search

--- components/search/SearchContext.tsx
+++ components/search/SearchContext.tsx
@@ -1,17 +1,50 @@
-import { api_link } from '../../lib/api';
+import { api_link } from '@/lib/api';
 import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
 
-interface SearchResult {
-  results: any[];
+interface Image {
+  quality: string;
+  link: string;
+}
+
+interface Song {
+  id: string;
+  title: string;
+  image: Image[];
+  album: string;
+  url: string;
+  type: string;
+  description: string;
+  position: number;
+  primaryArtists: string;
+  singers: string;
+  language: string;
+}
+
+interface Album {
+  id: string;
+  title: string;
+  image: Image[];
+  artist: string;
+  url: string;
+  type: string;
+  description: string;
+  position: number;
+  year: string;
+  songIds: string;
+  language: string;
+}
+
+interface SearchResult<T> {
+  results: T[];
   position: number;
 }
 
 interface SearchResults {
-  topQuery: SearchResult;
-  songs: SearchResult;
-  albums: SearchResult;
-  artists: SearchResult;
-  playlists: SearchResult;
+  topQuery: SearchResult<any>;
+  songs: SearchResult<Song>;
+  albums: SearchResult<Album>;
+  artists: SearchResult<any>;
+  playlists: SearchResult<any>;
 }
 
 interface SearchContextType {
@@ -25,7 +58,7 @@ interface SearchContextType {
 
 const SearchContext = createContext<SearchContextType | undefined>(undefined);
 
-export const useSearchContext = (): SearchContextType => {
+export const useSearchContext = () => {
   const context = useContext(SearchContext);
   if (!context) {
     throw new Error('useSearchContext must be used within a SearchProvider');
@@ -39,56 +72,53 @@ interface SearchProviderProps {
 
 export const SearchProvider: React.FC<SearchProviderProps> = ({ children }) => {
   const emptyResult: SearchResults = {
-    topQuery: {
-      results: [],
-      position: 0
+    "topQuery": {
+      "results": [],
+      "position": 0
     },
-    songs: {
-      results: [],
-      position: 1
+    "songs": {
+      "results": [],
+      "position": 1
     },
-    albums: {
-      results: [],
-      position: 2
+    "albums": {
+      "results": [],
+      "position": 2
     },
-    artists: {
-      results: [],
-      position: 3
+    "artists": {
+      "results": [],
+      "position": 3
     },
-    playlists: {
-      results: [],
-      position: 4
+    "playlists": {
+      "results": [],
+      "position": 4
     }
   };
-
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [searchResults, setSearchResults] = useState<SearchResults>(emptyResult);
-
   const ResetSearch = () => {
     setSearchQuery('');
     setSearchResults(emptyResult);
-  };
-
+  }
   useEffect(() => {
-    if (searchQuery && searchQuery.replace(/\s/g, "") !== "") {
-      const searchUrl = `${api_link}/search/all?query=${searchQuery}`;
+      if (searchQuery && searchQuery.replace(/\s/g, "") != "") {
+        var searchUrl = `${api_link}/search/all?query=${searchQuery}`;
 
-      fetch(searchUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.data) {
-            setSearchResults(data.data);
-          } else {
+        fetch(searchUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.data) {
+              setSearchResults(data.data);
+            } else {
+              setSearchResults(emptyResult);
+            }
+          })
+          .catch((error) => {
+            console.error('Error fetching search results:', error);
             setSearchResults(emptyResult);
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching search results:', error);
-          setSearchResults(emptyResult);
-        });
-    } else {
-      setSearchResults(emptyResult);
-    }
+          });
+      } else {
+        setSearchResults(emptyResult);
+      }
   }, [searchQuery]);
 
   return (

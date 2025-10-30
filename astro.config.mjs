27176c818ab29a44bfc72656809896eb2b036e5f
nandan-varma <nandanvarma.me@gmail.com>
2025-10-30 00:26:33 -0700

    vercel dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), svelte()],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });

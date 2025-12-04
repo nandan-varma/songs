29a63a095d4665c74c4b4a491364547de8ea8e5a
nandan-varma <nandanvarma.me@gmail.com>
2025-12-04 03:30:55 -0800

    bugfix : gitignore messed up, my bad

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['c.saavncdn.com', 'is1-ssl.mzstatic.com', 'i.scdn.co'],
+    remotePatterns: [
+      { 
+        protocol: 'https', 
+        hostname: 'c.saavncdn.com',
+        pathname: '/**',
+      }
+    ],
   },
 };
 

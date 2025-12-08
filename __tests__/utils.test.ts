2b9adf1e7352bc19475c92e7f295c755e8817206
nandan-varma <nandanvarma.me@gmail.com>
2025-12-07 18:42:40 -0800

    feat. : better testing

---
+++ __tests__/utils.test.ts
@@ -0,0 +1,13 @@
+import { cn } from "../lib/utils";
+
+describe("cn", () => {
+	it("merges class names correctly", () => {
+		expect(cn("class1", "class2")).toBe("class1 class2");
+	});
+
+	it("handles conditional classes", () => {
+		expect(cn("class1", true && "class2", false && "class3")).toBe(
+			"class1 class2",
+		);
+	});
+});

7ca443a8db9e94a58be67527f659ad020c77e9ca
nandan-varma <nandanvarma.me@gmail.com>
2026-04-03 15:08:49 -0700

    better navigation with auth

--- lib/external-db.ts
+++ lib/external-db.ts
@@ -1,10 +1,10 @@
-import "server-only";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "@/db/schema/auth";
 
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
-	throw new Error("DATABASE_URL is required");
+  throw new Error("DATABASE_URL is required");
 }
 
-export const external_db = drizzle(databaseUrl);
+export const external_db = drizzle(databaseUrl, { schema });
